75b70ff24e3b69eaac2e3a1d5f72b120a6073ad0
kdchk <kdchk.net@gmail.com>
2017-02-20 01:05:49 +0200

    [clean] up comment, update Readme for master

--- Client/Client.cs
+++ Client/Client.cs
@@ -13,29 +13,22 @@ namespace Client
             Console.WriteLine("[Client]");
 
             sckt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            Console.WriteLine("1");
             IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
-            Console.WriteLine("2");
             try
             {
                 sckt.Connect(localEndPoint);
-                Console.WriteLine("3");
             }
             catch (Exception)
             {
                 Console.WriteLine("Unable to connect to remote end point!");
-                Console.WriteLine("3.1");
                 Main(args);
             }
             Console.WriteLine("Enter message: ");
             string message = Console.ReadLine();
-            Console.WriteLine("4");
             byte[] data = Encoding.ASCII.GetBytes(message);
-            Console.WriteLine("5");
             sckt.Send(data);
-            Console.WriteLine("6");
             Console.WriteLine("Data was sent");
-            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Press any key to close...");
             Console.ReadKey();
             sckt.Shutdown(SocketShutdown.Send);
         }
--- ScktSrvr_SCtS/Server.cs
+++ ScktSrvr_SCtS/Server.cs
@@ -14,29 +14,19 @@ namespace ScktSrvr_SCtS
             Console.WriteLine("[Server]");
 
             sckt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            Console.WriteLine("1");
             sckt.Bind(new IPEndPoint(0, 1234));
-            Console.WriteLine("2");
             sckt.Listen(100);
-            Console.WriteLine("3");
 
             Socket accepted = sckt.Accept();
-            Console.WriteLine("4");
             Buffer = new byte[accepted.SendBufferSize];
-            Console.WriteLine("5");
             int bytesRead = accepted.Receive(Buffer);
-            Console.WriteLine("6");
 
             byte[] formatted = new byte[bytesRead];
-            Console.WriteLine("7");
             for (int i = 0; i < bytesRead; i++)
                 formatted[i] = Buffer[i];
-            Console.WriteLine("8");
 
             string strData = Encoding.ASCII.GetString(formatted);
-            Console.WriteLine("9");
             Console.WriteLine(strData);
-            Console.WriteLine("10");
             Console.ReadKey();
             sckt.Shutdown(SocketShutdown.Both);
             accepted.Shutdown(SocketShutdown.Both);
